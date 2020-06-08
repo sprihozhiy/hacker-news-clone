bb5ba21b7e56c64396b147f697b2f03c8cf44188
sprihozhiy <sprihozhiy@gmail.com>
2020-06-07 22:06:16 -0600

    initialized project

---
+++ README.md
@@ -0,0 +1,1 @@
+## Hacker News Clone - ReactJS with Firebase
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div style={{ margin: "0 auto", textAlign: "center" }}>
+      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
