9dd66535f002214c742086581a072a4df048fe57
sprihozhiy <sprihozhiy@gmail.com>
2020-06-14 22:41:52 -0600

    added firestore database to an app

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
+import { FirebaseContext } from "../../firebase";
 
 const INITIAL_STATE = {
   description: "",
@@ -8,13 +9,28 @@ const INITIAL_STATE = {
 };
 
 function CreateLink(props) {
+  const { firebase, user } = useContext(FirebaseContext);
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
     handleCreateLink
   );
   function handleCreateLink() {
-    console.log("link created");
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { description, url } = values;
+      const newLink = {
+        description,
+        url,
+        postedBy: { id: user.uid, name: user.displayName },
+        votes: [],
+        comments: [],
+        created: Date.now(),
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
   }
   return (
     <form onSubmit={handleSubmit} className="flex flex-column mt3">
@@ -32,7 +48,7 @@ function CreateLink(props) {
         name="url"
         placeholder="The URL for your link"
         autoComplete="off"
-        type="text"
+        type="url"
         onChange={handleChange}
         value={values.url}
         className={errors.url && "error-input"}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,14 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
+
 import firebaseConfig from "./config";
 
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
   //registering a user
   async register(name, email, password) {
