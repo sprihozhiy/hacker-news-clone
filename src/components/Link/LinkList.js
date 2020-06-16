20bb9d3a36065f2c9003b5614a4b98b6aee67de1
sprihozhiy <sprihozhiy@gmail.com>
2020-06-15 22:54:53 -0600

    added displaying data from database functionality

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,9 +1,12 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import FirebaseContext from "../../firebase/context";
+import LinkItem from "./LinkItem";
 
 function LinkList(props) {
   const { firebase } = useContext(FirebaseContext);
+  const [links, setLinks] = useState([]);
 
+  //fetching data from firestore database
   function getLinks() {
     firebase.db.collection("links").onSnapshot(handleSnapshot);
   }
@@ -12,13 +15,24 @@ function LinkList(props) {
     const links = snapshot.docs.map((doc) => {
       return { id: doc.id, ...doc.data() };
     });
-    console.log({ links });
+    setLinks(links);
   }
 
   useEffect(() => {
     getLinks();
   }, []);
-  return <div>LinkList</div>;
+  return (
+    <div>
+      {links.map((link, index) => (
+        <LinkItem
+          key={link.id}
+          showCount={true}
+          link={link}
+          index={index + 1}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
