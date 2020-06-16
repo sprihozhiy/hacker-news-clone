4fa6045856fdbc3dcffe8b92d26e8442fa72c0ec
sprihozhiy <sprihozhiy@gmail.com>
2020-06-15 22:21:13 -0600

    added fetching data from database

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import FirebaseContext from "../../firebase/context";
 
 function LinkList(props) {
+  const { firebase } = useContext(FirebaseContext);
+
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
+    console.log({ links });
+  }
+
+  useEffect(() => {
+    getLinks();
+  }, []);
   return <div>LinkList</div>;
 }
 
