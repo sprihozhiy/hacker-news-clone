26bf40a7e254cdeb10c4b744c9b3c63b78ec2d03
sprihozhiy <sprihozhiy@gmail.com>
2020-06-25 22:18:10 -0600

    added text search functionality

--- src/components/Link/SearchLinks.js
+++ src/components/Link/SearchLinks.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import FirebaseContext from "../../firebase/context";
+import LinkItem from "./LinkItem";
 
 function SearchLinks() {
-  return <div>SearchLinks</div>;
+  const { firebase } = useContext(FirebaseContext);
+  const [filteredLinks, setFilteredLinks] = useState([]);
+  const [links, setLinks] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  function handleSearch(e) {
+    e.preventDefault();
+    const query = filter.toLowerCase();
+    const matchedLinks = links.filter((link) => {
+      return (
+        link.description.toLowerCase().includes(query) ||
+        link.url.toLowerCase().includes(query) ||
+        link.postedBy.name.toLowerCase().includes(query)
+      );
+    });
+    setFilteredLinks(matchedLinks);
+  }
+
+  function getInitialLinks() {
+    firebase.db
+      .collection("links")
+      .get()
+      .then((snapshot) => {
+        const links = snapshot.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
+        setLinks(links);
+      });
+  }
+
+  useEffect(() => {
+    getInitialLinks();
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <div>
+          Search <input onChange={(e) => setFilter(e.target.value)} />
+          <button>Ok</button>
+        </div>
+      </form>
+      {filteredLinks.map((filteredLink, index) => (
+        <LinkItem
+          key={filteredLink.id}
+          showCount={false}
+          link={filteredLink}
+          index={index}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default SearchLinks;
